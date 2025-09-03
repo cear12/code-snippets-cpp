7e590b5b71aa57f790f598c6de6d8e3e63c47b7d
Ole <ole@MacBook-Air-Lana.local>
2025-09-03 12:11:25 +0700

    Added perfect-forward implementation example

---
+++ perfect-forward.h
@@ -0,0 +1,12 @@
+namespace std {
+    template <typename T>
+    constexpr T&& forward(typename remove_reference<T>::type& arg) noexcept {
+        return static_cast<T&&>(arg);
+    }
+
+    template <typename T>
+    constexpr T&& forward(typename remove_reference<T>::type&& arg) noexcept {
+        static_assert(!is_lvalue_reference<T>::value, "template argument substituting T is an lvalue reference type");
+        return static_cast<T&&>(arg);
+    }
+}
